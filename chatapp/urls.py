733ad894cecf6b0e004e39b3d4a4f815db5c3fa6
farbodgerami <fb.gerami@gamil.com>
2026-06-11 14:14:31 +0330

    update cicd

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -18,7 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import JsonResponse
+
+def health(request):
+    return JsonResponse({"status": "ok"})
+
 urlpatterns = [
     path('admin/', admin.site.urls),   path("", include("chat.urls")),
+     path("health/", health),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(
     settings.STATIC_URL, document_root=settings.STATIC_ROOT)
